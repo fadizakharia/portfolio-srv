8af6a8c0fb7f218b4d69f01bd860391c772f3b04
fadi <fadi.zakharia@icloud.com>
2022-05-27 16:17:12 +0300

    fixed image upload persistence

--- config/plugins.js
+++ config/plugins.js
@@ -3,13 +3,31 @@ module.exports = ({ env }) => ({
     config: {
       provider: "amazon-ses",
       providerOptions: {
-        key: env("AWS_SES_KEY"),
-        secret: env("AWS_SES_SECRET"),
+        key: env("AWS_KEY"),
+        secret: env("AWS_SECRET"),
         amazon: "https://email.us-east-1.amazonaws.com",
       },
       settings: {
         defaultFrom: "fadi.zakharia@icloud.com",
         defaultReplyTo: "fadi.zakharia@icloud.com",
+      },
+    },
+  },
+  upload: {
+    config: {
+      provider: "aws-s3",
+      providerOptions: {
+        accessKeyId: env("AWS_KEY"),
+        secretAccessKey: env("AWS_SECRET"),
+        region: env("AWS_REGION"),
+        params: {
+          Bucket: env("AWS_BUCKET"),
+        },
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
       },
     },
   },
